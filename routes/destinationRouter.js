375f08b471452e962a9d718bfd429af69ec7317f
Green-Eggs-And-SAM <sam.john.alexander@gmail.com>
2024-06-04 10:27:37 -0700

    removed node modules. endpoint /names working

--- routes/destinationRouter.js
+++ routes/destinationRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
 const destinations = require("../data/destinations.json");
 
 router.get("/", (req, res) => {
@@ -7,17 +8,20 @@ router.get("/", (req, res) => {
 });
 
 router.get("/names", (req, res) => {
-    const dataArr = JSON.parse(destinations);
+    const dataArr = JSON.parse(fs.readFileSync("./data/destinations.json"));
     const arr = dataArr.map((destination) => destination.name);
-    console.log(arr);
-    res.json(images);
+    res.json(JSON.stringify(arr));
 });
 
 router.get("/:name", (req, res) => {
     const foundDestination = destinations.find(
-        (item) => item.name === req.params.name
+        (item) =>
+            item.name.toLowerCase() === req.params.name.toLocaleLowerCase()
     );
+    console.log(foundDestination);
     // you could include an if statement to check if the foundImage is undefined
     // if the foundImage is undefined use res.json or res.send to send back an error message
     res.json(foundDestination);
 });
+
+module.exports = router;
