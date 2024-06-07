16b65fa800ab0b0b51eda5e63a8621f49c424ff0
Green-Eggs-And-SAM <sam.john.alexander@gmail.com>
2024-06-06 22:15:43 -0700

    aukland, paris, mexico added

--- routes/destinationRouter.js
+++ routes/destinationRouter.js
@@ -10,7 +10,7 @@ router.get("/", (req, res) => {
 router.get("/names", (req, res) => {
     const dataArr = JSON.parse(fs.readFileSync("./data/destinations.json"));
     const arr = dataArr.map((destination) => destination.name);
-    res.json(JSON.stringify(arr));
+    res.json(arr);
 });
 
 router.get("/:name", (req, res) => {
