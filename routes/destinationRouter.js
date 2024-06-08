8645347fbf255dfa114a377c9c9bca80ae44eba2
Green-Eggs-And-SAM <sam.john.alexander@gmail.com>
2024-06-07 19:07:34 -0700

    bug fix: correct paris url

--- routes/destinationRouter.js
+++ routes/destinationRouter.js
@@ -7,10 +7,21 @@ router.get("/", (req, res) => {
     res.json(destinations);
 });
 
-router.get("/names", (req, res) => {
+router.get("/names-flags-list", (req, res) => {
     const dataArr = JSON.parse(fs.readFileSync("./data/destinations.json"));
-    const arr = dataArr.map((destination) => destination.name);
-    res.json(arr);
+
+    const names = dataArr.map((destination) => destination.name);
+    const flags = dataArr.map((destination) => destination.flag);
+
+    let objs = [];
+    for (let i = 0; i < names.length; i++) {
+        const obj = {};
+        obj.name = names[i];
+        obj.flag = flags[i];
+        objs.push(obj);
+    }
+    console.log(objs);
+    res.json(objs);
 });
 
 router.get("/:name", (req, res) => {
@@ -19,9 +30,21 @@ router.get("/:name", (req, res) => {
             item.name.toLowerCase() === req.params.name.toLocaleLowerCase()
     );
     console.log(foundDestination);
-    // you could include an if statement to check if the foundImage is undefined
-    // if the foundImage is undefined use res.json or res.send to send back an error message
-    res.json(foundDestination);
+    let obj = {};
+    obj.name = foundDestination.name;
+    obj.flag = foundDestination.flag;
+    obj.landscape = foundDestination.landscape;
+
+    res.json(obj);
+});
+
+router.get("/:name/video", (req, res) => {
+    const foundDestination = destinations.find(
+        (item) =>
+            item.name.toLowerCase() === req.params.name.toLocaleLowerCase()
+    );
+    console.log(foundDestination.video);
+    res.json(foundDestination.video);
 });
 
 module.exports = router;
